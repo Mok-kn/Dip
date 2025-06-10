7141c9eae672f4b92bde871367e71859bb6c5ae0
Mok-kn <23011211151@stu.xidian.edu.cn>
2025-06-10 12:30:37 +0800

    Add files via upload

---
+++ configs/faster_rcnn_r50_distill_r18_fpn_SSDD.py
@@ -0,0 +1,157 @@
+_base_ = [
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+temp=0.5
+# alpha_fgd=0
+alpha_fgd=0.00005
+# beta_fgd=0.000025
+beta_fgd=0.000035
+gamma_fft=0.00000001
+
+lambda_mask = 0.25
+
+distiller = dict(
+    type='DetectionDistiller',
+    # teacher_pretrained = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r101_fpn_2x_coco/faster_rcnn_r101_fpn_2x_coco_bbox_mAP-0.398_20200504_210455-1d2dac9c.pth',
+    teacher_pretrained = 'A_work_dirs/Work1/faster_rcnn/r50_SSDD/epoch_22.pth',
+    init_student = True,
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                   ]
+    )
+
+student_cfg = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/configs/AA_faster_rcnn/faster_rcnn_r18_fpn_SSDD.py'
+teacher_cfg = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/configs/AA_faster_rcnn/faster_rcnn_r50_fpn_SSDD.py'
+
+dataset_type = 'CocoDataset'
+data_root = 'data_SSDD/coco_SSDD/'
+classes = ('ship',)
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1000, 600),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        classes =classes ,
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes =classes ,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes =classes ,
+        ann_file=data_root + 'annotations/instances_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[16, 20])
+runner = dict(type='EpochBasedRunner', max_epochs=24)
+
+# work_dir = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/A_work_dirs/Work1_1/faster_rcnn1/r50disr18_SSDD'
+
+work_dir = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/A_work_dirs/20250425/fasterrcnn'
+
---
+++ configs/retinanet_r50_distill_r18_fpn_HRSID.py
@@ -0,0 +1,153 @@
+_base_ = [
+    '../_base_/datasets/coco_detection.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+# model settings
+find_unused_parameters=True
+temp=0.5
+# alpha_fgd=0.00005
+alpha_fgd=0.00004
+# beta_fgd=0.000035
+beta_fgd=0.000025
+gamma_fft=0.00000001
+
+lambda_mask = 0.25
+
+distiller = dict(
+    type='DetectionDistiller',
+    # teacher_pretrained = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r101_fpn_2x_coco/faster_rcnn_r101_fpn_2x_coco_bbox_mAP-0.398_20200504_210455-1d2dac9c.pth',
+    teacher_pretrained = 'A_work_dirs/Work1/retinanet/r50_HRSID/epoch_24.pth',
+    init_student = True,
+    distill_cfg = [ dict(student_module = 'neck.fpn_convs.3.conv',
+                         teacher_module = 'neck.fpn_convs.3.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_3',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.2.conv',
+                         teacher_module = 'neck.fpn_convs.2.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_2',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.1.conv',
+                         teacher_module = 'neck.fpn_convs.1.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_1',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                    dict(student_module = 'neck.fpn_convs.0.conv',
+                         teacher_module = 'neck.fpn_convs.0.conv',
+                         output_hook = True,
+                         methods=[dict(type='FeatureLoss',
+                                       name='loss_fgd_fpn_0',
+                                       student_channels = 256,
+                                       teacher_channels = 256,
+                                       temp = temp,
+                                       alpha_fgd=alpha_fgd,
+                                       beta_fgd=beta_fgd,
+                                       gamma_fft=gamma_fft,
+                                       lambda_mask = lambda_mask,
+                                       )
+                                ]
+                        ),
+                   ]
+    )
+
+student_cfg = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/configs/AA_retinanet/retinanet_r18_fpn_HRSID.py'
+teacher_cfg = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/configs/AA_retinanet/retinanet_r50_fpn_HRSID.py'
+
+dataset_type = 'CocoDataset'
+data_root = 'data/HRSID/HRSID_JPG'
+classes = ('ship',)
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(800, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(800, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        classes =classes ,
+        type=dataset_type,
+        ann_file=data_root + '/annotations/train2017.json',
+        img_prefix=data_root + '/JPEGImages/',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        classes =classes ,
+        ann_file=data_root + '/annotations/test2017.json',
+        img_prefix=data_root + '/JPEGImages/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes =classes ,
+        ann_file=data_root + '/annotations/test2017.json',
+        img_prefix=data_root + '/JPEGImages/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[16, 20])
+runner = dict(type='EpochBasedRunner', max_epochs=24)
+
+work_dir = '/media/yangxilab/DiskB/sjc/practice/FGD/FGD-master/mmdetection/A_work_dirs/Work1_1/retinanet/r50disr18_HRSID'
